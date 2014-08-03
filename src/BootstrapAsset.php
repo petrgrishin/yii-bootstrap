9fe3f9b4e2506140554bda161e7605fe51ab604b
Petr Grishin <petr.grishin@grishini.ru>
2014-08-03 14:26:31 +0400

    Реализация подключения стилий и скриптов

---
+++ src/BootstrapAsset.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @author Petr Grishin <petr.grishin@grishini.ru>
+ */
+
+namespace PetrGrishin\Asset\Bootstrap;
+
+
+use Yii;
+use CApplicationComponent;
+use CClientScript;
+use CAssetManager;
+
+class BootstrapAsset extends CApplicationComponent {
+
+    public function init() {
+        parent::init();
+        $assetPath = $this->getAssetManager()->publish('/vendor/twbs/bootstrap/dist/');
+        $this->getClientScript()->registerCssFile($assetPath . 'css/bootstrap.min.css');
+        $this->getClientScript()->registerCssFile($assetPath . 'css/bootstrap-theme.min.css');
+        $this->getClientScript()->registerScriptFile($assetPath . 'css/bootstrap.min.js');
+    }
+
+    /**
+     * @return CAssetManager
+     */
+    protected function getAssetManager() {
+        return $this->getApp()->getComponent('assetManager');
+    }
+
+    /**
+     * @return CClientScript
+     */
+    protected function getClientScript() {
+        return $this->getApp()->getComponent('clientScript');
+    }
+
+    protected function getApp() {
+        return Yii::app();
+    }
+}
+ 
